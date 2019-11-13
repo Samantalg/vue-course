c334c556e57e0cabe924ddf3dc9d5ae2e908ffac
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 09:20:04 +0000

    directives

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 // creamos la instancia de vue
-new Vue({
+const vm = new Vue({ //si ponemos vm = ... podemos acceder desde fuera a la instacia para cambiar las propiedades
     el: 'main', // de esta manera accede a lo que esté dentro de <main></main>
     data: {
-        mensaje: 'Hola Mundo!',
+        conectado: true,
+        edad: 44
     }
 });
