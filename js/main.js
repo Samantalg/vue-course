2a3401634180306eeac8f84b321e251d59b54eda
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 15:50:13 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,30 +1,43 @@
-Vue.component('autor', {
-    props: ['nombre'],
-    mounted() {
-        console.log(this.nombre);
-    },
-    template: `
-    <span>
-      <h2>El nombre de la autora es {{ nombre }}</h2>
-      <button @click="cambiarProp">Cambiar Prop</button>
-      </span>
-    `,
-    methods: {
-        cambiarProp() {
-            this.nombre = this.nombre.toUpperCase();
-            console.log(this.nombre);
-        }
+Vue.component('candidato', {
+    props: {
+        nombre: {
+            type: String,
+            required: true
+        },
+        correo: {
+            type: String,
+            required: true
+        },
+        imagen: {
+            type: String,
+            required: true
+        },
+        location: {
+            type: Object,
+            default () {
+                return {
+                    ciudad: 'Madrid'
+                }
+            }
+        },
+        template: '#candidato-template',
     }
 });
 
-/*
-Cada componente es una isla, aislada del resto, por lo que se necesitan formas de pasar información desde la instancia o
-componente parent al componente hijo (One-way data flow). Ese es el objetivo de las propiedades de los componentes.
-*/
-
 const vm1 = new Vue({
     el: 'main',
+    mounted() {
+        this.obtenerCandidatos();
+    },
     data: {
-        autor: 'Samanta Lara'
+        candidatos: []
+    },
+    methods: {
+        obtenerCandidatos() {
+            axios.get('https://randomuser.me/api/?results=5')
+                .then((respuesta) => {
+                    this.candidatos = respuesta.data.results;
+                })
+        }
     }
 });
