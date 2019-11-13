cb4c9ba9d31f38c96bab906e5ed54e4c3074c026
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 21:24:01 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,66 +1,30 @@
-const eventBus = new Vue(); // utilizamos esta instancia como puente entre componentes hermanos
-
-Vue.component('listado-productos', {
-    props: ['productos'],
-    template: `
-    <section>
-      <ul>
-        <li v-for="producto in productos">
-          {{ producto.nombre }} -- <small>precio: {{ producto.precio.toFixed(2) }}</small>
-          <button @click="eliminarProducto(producto.precio)">-</button>
-          <button @click="anadirProducto(producto.precio)">+</button>
-        </li>
+Vue.component('lista-tareas', {
+    props: ['tareas'],
+    template: ` 
+    <div>
+      <h1><slot></slot></h1>
+      <ul v-for="tarea in tareas">
+      <mi-tarea v-bind:tarea="tarea"></mi-tarea>
       </ul>
-    </section>
-    `,
-    methods: {
-        anadirProducto(precio) {
-            eventBus.$emit('añadir', precio);
-        },
-        eliminarProducto(precio) {
-            eventBus.$emit('quitar', precio);
-        }
-    }
+    </div>
+    `
 });
 
-Vue.component('carrito-compra', {
+Vue.component('mi-tarea', {
+    props: ['tarea'],
     template: `
-    <section>
-      <h1>{{ cantidadProductos }}</h1>
-      <h3>{{ total }} productos</h3>
-    </section>
-    `,
-    data() {
-        return {
-            cantidadProductos: 0,
-            total: 0
-        }
-    },
-    created() {
-        eventBus.$on('añadir', (precio) => {
-                if (this.total >= 0) {
-                    this.total += precio;
-                    this.cantidadProductos++;
-                }
-            }),
-            eventBus.$on('quitar', (precio) => {
-                if (this.total > 0) {
-                    this.total -= precio;
-                    this.cantidadProductos--;
-                }
-            });
-    }
+    <li>{{ tarea }}</li>
+    `
 });
 
 const vm1 = new Vue({
     el: 'main',
     data: {
-        productos: [
-            { nombre: 'Libro', precio: 39 },
-            { nombre: 'Portátil', precio: 1300 },
-            { nombre: 'Café', precio: 5 },
-            { nombre: 'Chocolate', precio: 3 },
-            { nombre: 'Bolígrafo', precio: 1 }
+        tareas: [
+            'Primera tarea',
+            'Segunda tarea',
+            'Tercera tarea',
+            'Cuarta tarea',
         ]
     }
 });
