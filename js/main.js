5c0e3ad5fef8bfbda2e92a99c149db53f5089887
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 20:58:46 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,23 +1,66 @@
-/*
-con los custom events de Vue.js podemos pasar información de los hijos a los padres.
-*/
+const eventBus = new Vue(); // utilizamos esta instancia como puente entre componentes hermanos
 
-Vue.component('alerta', {
-    props: ['tipo', 'posicion'],
-    template: '#alerta-template',
+Vue.component('listado-productos', {
+    props: ['productos'],
+    template: `
+    <section>
+      <ul>
+        <li v-for="producto in productos">
+          {{ producto.nombre }} -- <small>precio: {{ producto.precio.toFixed(2) }}</small>
+          <button @click="eliminarProducto(producto.precio)">-</button>
+          <button @click="anadirProducto(producto.precio)">+</button>
+        </li>
+      </ul>
+    </section>
+    `,
     methods: {
-        ocultarWidget() {
-            console.log('clickado');
-            this.$emit('ocultar');
+        anadirProducto(precio) {
+            eventBus.$emit('añadir', precio);
+        },
+        eliminarProducto(precio) {
+            eventBus.$emit('quitar', precio);
         }
+    }
+});
+
+Vue.component('carrito-compra', {
+    template: `
+    <section>
+      <h1>{{ cantidadProductos }}</h1>
+      <h3>{{ total }} productos</h3>
+    </section>
+    `,
+    data() {
+        return {
+            cantidadProductos: 0,
+            total: 0
+        }
+    },
+    created() {
+        eventBus.$on('añadir', (precio) => {
+                if (this.total >= 0) {
+                    this.total += precio;
+                    this.cantidadProductos++;
+                }
+            }),
+            eventBus.$on('quitar', (precio) => {
+                if (this.total > 0) {
+                    this.total -= precio;
+                    this.cantidadProductos--;
+                }
+            });
     }
 });
 
 const vm1 = new Vue({
     el: 'main',
     data: {
-        mostrarExito: false,
-        mostrarAdvertencia: false,
-        mostrarError: false
+        productos: [
+            { nombre: 'Libro', precio: 39 },
+            { nombre: 'Portátil', precio: 1300 },
+            { nombre: 'Café', precio: 5 },
+            { nombre: 'Chocolate', precio: 3 },
+            { nombre: 'Bolígrafo', precio: 1 }
+        ]
     }
 });
