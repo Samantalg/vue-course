4989c024a32512212674a7ab7881dc300abcde75
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 14:59:16 +0000

    templates component

--- js/main.js
+++ js/main.js
@@ -1,31 +1,28 @@
-/* Vue.component('mis-tareas', {
-    props: ['_tareas'], // tengo que definirlo para poder pasarlo en <mis-tareas v-bind:tareas="tareas">
-    template: `
-    <ul>
-      <li v-for="tarea in _tareas">{{ tarea.title }}</li>
-    </ul>
-    `,
-});
- */
-Vue.component('mi-titulo', {
-    props: ['_titulo'],
-    template: `
-  <h2>{{ _titulo }}</h2>
-  `,
-});
-
-Vue.component('mis-tareas', {
-    template: `<ul><li v-for="tarea in tareas">{{ tarea.title }}</li></ul>`,
-    mounted() {
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-            .then((respuesta) => {
-                console.log(respuesta);
-                this.tareas = respuesta.data;
-            });
+Vue.component('elegir-ganador', {
+    props: ['listado'],
+    /* templat: '#elegir-ganador-template', */
+    /* template `
+    <div>
+      <h4 v-if="ganador">El ganador es {{ganador}}</h4>
+      <template v-else>
+      <h4>Participantes</h4>
+      <ul>
+      <li v-for="persona in listado">{{persona}}</li></ul>
+      </template>
+      <button @click="elegirGanador">Jugar</button>
+    </div>
+    ` */
+    methods: {
+        elegirGanador() {
+            let cantidad = this.participantes.length;
+            let indice = Math.floor((Math.random() * cantidad)); // nos va a devolver entre 1 y 4
+            this.ganador = this.participantes[indice - 1];
+        }
     },
     data() {
         return {
-            tareas: [],
+            ganador: false,
+            participantes: this.listado
         }
     }
 });
@@ -33,27 +30,8 @@ Vue.component('mis-tareas', {
 const vm1 = new Vue({
     el: 'main',
     data: {
-        titulo: 'Mis tareas'
-    }
-    /* data: {
-        tareas: [],
-        tareasLocales: [
-            { title: 'Hacer la compra' },
-            { title: 'Aprender Vue' }
+        personas: [
+            'Juan', 'Alicia', 'Pedro', 'Maria'
         ]
-    },
-    mounted() {
-        console.log('Instancia montada');
-        this.cargarPersonas();
-    },
-    methods: {
-        cargarPersonas() {
-            axios.get('https://jsonplaceholder.typicode.com/todos')
-                .then((respuesta) => {
-                    console.log(respuesta);
-                    this.tareas = respuesta.data;
-                })
-
-        }
-    } */
+    }
 });
