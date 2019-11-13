1f4b4cb7dc1c6b3577e7f7c0fa7948b7790ec1b6
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 16:15:33 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,43 +1,26 @@
-Vue.component('candidato', {
-    props: {
-        nombre: {
-            type: String,
-            required: true
-        },
-        correo: {
-            type: String,
-            required: true
-        },
-        imagen: {
-            type: String,
-            required: true
-        },
-        location: {
-            type: Object,
-            default () {
-                return {
-                    ciudad: 'Madrid'
-                }
-            }
-        },
-        template: '#candidato-template',
-    }
+/*Si las propiedades en los componentes permiten personalizar la lógica de cada instancia, los slots en Vue.js permiten que parte del template
+sea dinámico, único para cada instancia.
+*/
+
+Vue.component('alerta', {
+    props: ['tipo', 'posicion'],
+    template: `
+      <section class="alerta" v-bind:class="[tipo, posicion]">
+        <header class="alerta__header">
+        <slot name="header">Hola</slot>
+        </header>
+        <div class="alerta__contenido">
+          <slot>jlsfñjdsfsfk fdsfjps kpfpkf dfjspñsfpñfsk</slot>
+        </div>
+        <footer class="alerta__footer">
+        <slot name="footer">Footer</slot>
+        </footer>
+      </section>     
+    `
 });
+
+/*slot anonimo solo puede haber uno */
 
 const vm1 = new Vue({
     el: 'main',
-    mounted() {
-        this.obtenerCandidatos();
-    },
-    data: {
-        candidatos: []
-    },
-    methods: {
-        obtenerCandidatos() {
-            axios.get('https://randomuser.me/api/?results=5')
-                .then((respuesta) => {
-                    this.candidatos = respuesta.data.results;
-                })
-        }
-    }
 });
