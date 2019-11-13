e459179fb1a3b6531761352cea2bfcb8653bc7bb
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 16:27:17 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
-/*Si las propiedades en los componentes permiten personalizar la lógica de cada instancia, los slots en Vue.js permiten que parte del template
-sea dinámico, único para cada instancia.
-*/
-
-Vue.component('alerta', {
-    props: ['tipo', 'posicion'],
-    template: `
-      <section class="alerta" v-bind:class="[tipo, posicion]">
-        <header class="alerta__header">
-        <slot name="header">Hola</slot>
-        </header>
-        <div class="alerta__contenido">
-          <slot>jlsfñjdsfsfk fdsfjps kpfpkf dfjspñsfpñfsk</slot>
-        </div>
-        <footer class="alerta__footer">
-        <slot name="footer">Footer</slot>
-        </footer>
-      </section>     
-    `
+Vue.component('mis-tareas', {
+    props: ['listado'],
+    template: '#mis-tareas-template'
 });
-
-/*slot anonimo solo puede haber uno */
 
 const vm1 = new Vue({
     el: 'main',
+    data: {
+        tareas: [{
+                titulo: 'Sacar al perro'
+            },
+            {
+                titulo: 'Aprender Vue'
+            },
+            {
+                titulo: 'Hacer la cama'
+            },
+        ]
+    }
 });
