f6f482a7980e8598dafa2c976a325075db855502
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 15:23:13 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,37 +1,30 @@
-Vue.component('elegir-ganador', {
-    props: ['listado'],
-    /* templat: '#elegir-ganador-template', */
-    /* template `
-    <div>
-      <h4 v-if="ganador">El ganador es {{ganador}}</h4>
-      <template v-else>
-      <h4>Participantes</h4>
-      <ul>
-      <li v-for="persona in listado">{{persona}}</li></ul>
-      </template>
-      <button @click="elegirGanador">Jugar</button>
-    </div>
-    ` */
-    methods: {
-        elegirGanador() {
-            let cantidad = this.participantes.length;
-            let indice = Math.floor((Math.random() * cantidad)); // nos va a devolver entre 1 y 4
-            this.ganador = this.participantes[indice - 1];
-        }
+Vue.component('autor', {
+    props: ['nombre'],
+    mounted() {
+        console.log(this.nombre);
     },
-    data() {
-        return {
-            ganador: false,
-            participantes: this.listado
+    template: `
+    <span>
+      <h2>El nombre de la autora es {{ nombre }}</h2>
+      <button @click="cambiarProp">Cambiar Prop</button>
+      </span>
+    `,
+    methods: {
+        cambiarProp() {
+            this.nombre = this.nombre.toUpperCase();
+            console.log(this.nombre);
         }
     }
 });
 
+/*
+Cada componente es una isla, aislada del resto, por lo que se necesitan formas de pasar información desde la instancia o
+componente parent al componente hijo (One-way data flow). Ese es el objetivo de las propiedades de los componentes.
+*/
+
 const vm1 = new Vue({
     el: 'main',
     data: {
-        personas: [
-            'Juan', 'Alicia', 'Pedro', 'Maria'
-        ]
+        autor: 'Samanta Lara'
     }
 });
