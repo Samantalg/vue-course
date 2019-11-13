1463823b0c6473dba92edca1a90d2ad8c2638ed4
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 10:54:31 +0000

    filter

--- js/main.js
+++ js/main.js
@@ -1,41 +1,42 @@
+Vue.filter('alReves', (valor) => valor.split('').reverse().join('')); // definido antes e la instancia vue para que funcione
+/* A diferencia de Vue1, en Vue2 no disponemos de los filtros filterBy y orderBy (entre otros).
+Aunque hay quien ha tratado de portarlos, lo mejor esaprender como utilizar las propiedades computadas,
+filtros personalizados y librerías de utilidades (lodash, ...).*/
+
 // creamos la instancia de vue
-const vm = new Vue({ //si ponemos vm = ... podemos acceder desde fuera a la instacia para cambiar las propiedades
+new Vue({ //si ponemos const vm = ... podemos acceder desde fuera a la instacia para cambiar las propiedades
     el: 'main',
     data: {
-        nuevaTarea: null,
-        tareas: [
-            { titulo: 'Hacer la compra', prioridad: false, antiguedad: 23 },
-            { titulo: 'Terminar el proyecto de Angular', prioridad: true, antiguedad: 5 },
-            { titulo: 'Aprender Vue', prioridad: true, antiguedad: 2 }
-
-        ],
-        mensaje: 'Hola Mundo'
+        busqueda: '',
+        minimo: 5,
+        juegos: [{
+                titulo: 'Juego 1',
+                genero: 'FPS',
+                puntuacion: 9
+            },
+            {
+                titulo: 'Juego 2',
+                genero: 'Estategia',
+                puntuacion: 10
+            },
+            {
+                titulo: 'Juego 3',
+                genero: 'Estategia',
+                puntuacion: 4
+            }
+        ]
     },
 
-    methods: {
-        agregarTarea() {
-            // this, hace referencia a la instancia vue
-            this.tareas.push(this.nuevaTarea);
-            this.nuevaTarea = null;
+    computed: {
+        mejoresJuegos() {
+            return this.juegos.filter((juego) => juego.puntuacion > this.minimo);
+        },
+        juegosOrdenados() { // de menos a mayor
+            return this.juegos.sort((a, b) => a.puntuacion - b.puntuacion);
+        },
+        buscarJuego() {
+            return this.juegos.filter((juego) => juego.titulo.includes(this.busqueda));
         }
-    },
 
-    computed: { // propiedades computadas
-        mensajeAlReves() {
-            return this.mensaje.split('').reverse().join('') // separa en letras, le da la vuelta al array y vuelve a unir la palabra
-        },
-        tareasConPrioridad() {
-            return this.tareas.filter((tarea) => tarea.prioridad);
-        },
-        tareasPorAntiguedad() {
-            return this.sort((a, b) => b.antiguedad - a.antiguedad);
-        }
     }
 });
-
-// Vanilla Javascript
-/* function agregarTarea() {
-    const input = document.querySelector('input[type="text"]');
-    vm.tareas.push(input.value);
-    input.value = '';
-} */
