b088d83646c27c592244fda4639fc8e0d8fedf48
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-13 20:12:55 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-Vue.component('mis-tareas', {
-    props: ['listado'],
-    template: '#mis-tareas-template'
+/*
+con los custom events de Vue.js podemos pasar información de los hijos a los padres.
+*/
+
+Vue.component('alerta', {
+    props: ['tipo', 'posicion'],
+    template: '#alerta-template',
+    methods: {
+        ocultarWidget() {
+            console.log('clickado');
+            this.$emit('ocultar');
+        }
+    }
 });
 
 const vm1 = new Vue({
     el: 'main',
     data: {
-        tareas: [{
-                titulo: 'Sacar al perro'
-            },
-            {
-                titulo: 'Aprender Vue'
-            },
-            {
-                titulo: 'Hacer la cama'
-            },
-        ]
+        mostrarExito: false,
+        mostrarAdvertencia: false,
+        mostrarError: false
     }
 });
