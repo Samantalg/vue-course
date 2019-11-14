de7929a1836fb97d9de041a07226d7600692a2da
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-14 09:56:57 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
+Vue.component('usuarios', {
+    template: '#usuarios-template',
+    mounted() {
+        axios.get('https://randomuser.me/api/?results=10')
+            .then((respuesta) => {
+                this.usuarios = respuesta.data.results.map((usuario) => {
+                    return {
+                        nombre: `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`,
+                        correo: usuario.email,
+                        foto: usuario.picture.medium
+                    }
+                });
+            })
+    },
+    data() {
+        return {
+            usuarios: [],
+            busqueda: ''
+        }
+    },
+    computed: {
+        filtrarUsuarios() {
+            return this.usuarios.filter((usuario) => usuario.nombre.includes(this.busqueda))
+        }
+    }
+});
+
+Vue.component('usuario', {
+    props: ['datos'],
+    template: '#usuario-template'
+});
+
+/* Vue.component('busqueda', {
+    template: `<input type="text">`
+}) */
+
+
+
 new Vue({
     el: 'main',
-    data: {
-        password: 'esQWERT765!'
-    }
 });
