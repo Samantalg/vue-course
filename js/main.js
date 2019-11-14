0526aeca74892b4b4fbd2f0e8d8f2b4cbe49d303
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-14 08:44:55 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
+/* Con los componentes dinámicos de Vue.js tenemos un único punto de montaje universal para cualquier cantidad de componentes.
+De esta forma podemos decidir qué componente está montado en cada momento. */
+
 Vue.component('lista-tareas', {
-    props: ['tareas'],
     template: ` 
     <div>
       <h1><slot></slot></h1>
-      <ul v-for="tarea in tareas">
-      <mi-tarea v-bind:tarea="tarea"></mi-tarea> <!--Componente dentro de componente-->
+      <ul>
+        <mi-tarea v-for="tarea in tareas" v-bind:tarea="tarea"></mi-tarea> <!--Componente dentro de componente-->
       </ul>
     </div>
-    `
+    `,
+    data() {
+        return {
+            tareas: [
+                'Primera tarea',
+                'Segunda tarea',
+                'Tercera tarea',
+                'Cuarta tarea',
+            ]
+        }
+    }
 });
 
 Vue.component('mi-tarea', {
@@ -17,14 +29,17 @@ Vue.component('mi-tarea', {
     `
 });
 
+Vue.component('contacto', {
+    template: `<div><a href="mailto:contacto@vue.es">contacto@vue.es</a><hr></div>`
+});
+
+Vue.component('bio', {
+    template: `<div><p>Esta es mi biografía</p></div>`
+})
+
 const vm1 = new Vue({
     el: 'main',
     data: {
-        tareas: [
-            'Primera tarea',
-            'Segunda tarea',
-            'Tercera tarea',
-            'Cuarta tarea',
-        ]
+        seleccionado: 'lista-tareas'
     }
 });
