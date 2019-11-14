6f5119d1ba5b44730df7a15c5f89cf8e6d706c07
Samanta Belen Lara <slaragiannone@gmail.com>
2019-11-14 09:15:07 +0000

    ~

--- js/main.js
+++ js/main.js
@@ -1,45 +1,28 @@
-/* Con los componentes dinámicos de Vue.js tenemos un único punto de montaje universal para cualquier cantidad de componentes.
-De esta forma podemos decidir qué componente está montado en cada momento. */
-
-Vue.component('lista-tareas', {
-    template: ` 
-    <div>
-      <h1><slot></slot></h1>
-      <ul>
-        <mi-tarea v-for="tarea in tareas" v-bind:tarea="tarea"></mi-tarea> <!--Componente dentro de componente-->
-      </ul>
-    </div>
-    `,
+Vue.component('password', {
+    props: ['password'],
+    template: `<input v-bind:value="password" @input="comprobarContrasenia($event.target.value)" ref="pass">`,
+    methods: {
+        comprobarContrasenia(contrasenia) {
+            if (this.noValidas.includes(contrasenia)) {
+                this.$refs.pass.value = contrasenia = '';
+            }
+            this.$emit('input', contrasenia);
+        }
+    },
     data() {
         return {
-            tareas: [
-                'Primera tarea',
-                'Segunda tarea',
-                'Tercera tarea',
-                'Cuarta tarea',
-            ]
+            noValidas: ['admin', 'root', '123456', 'abc']
         }
     }
-});
-
-Vue.component('mi-tarea', {
-    props: ['tarea'],
-    template: `
-    <li>{{ tarea }}</li>
-    `
-});
-
-Vue.component('contacto', {
-    template: `<div><a href="mailto:contacto@vue.es">contacto@vue.es</a><hr></div>`
-});
-
-Vue.component('bio', {
-    template: `<div><p>Esta es mi biografía</p></div>`
 })
 
-const vm1 = new Vue({
+/* template: `<input v-model="password">` */
+/* $event.target.value -> lo que haya escrito el usuario en el input */
+/*ref="pass" identificador de lo que se ha escrito en el input; this.$refs.& devuelve el elemento & del dom */
+
+new Vue({
     el: 'main',
     data: {
-        seleccionado: 'lista-tareas'
+        password: 'esQWERT765!'
     }
 });
